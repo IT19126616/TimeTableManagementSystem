175374dab3da212abb7bb7a5892d415b8e8c3e98
SChanakya <chanakyachana@gmail.com>
2021-03-05 00:02:10 +0530

    1.7 time table(section 6)

--- TimeTableManagementSystem/src/section6/TimeTable_location.java
+++ TimeTableManagementSystem/src/section6/TimeTable_location.java
@@ -1,4 +1,4 @@
-package Member4;
+package section6;
 
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -9,19 +9,22 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
-import javax.swing.JTextField;
+import javax.swing.JTable;
 import javax.swing.SwingConstants;
-import javax.swing.JList;
-import javax.swing.JComboBox;
+import javax.swing.table.DefaultTableModel;
 
-public class AddSessionRooms {
+import Member4.Main4;
 
-	private JFrame frame;
+public class TimeTable_location {
+
+	private JFrame frame;	private JTable table;
+
 
 	/**
 	 * Launch the application.
@@ -30,7 +33,7 @@ public class AddSessionRooms {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					AddSessionRooms window = new AddSessionRooms();
+					TimeTable_location window = new TimeTable_location();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,7 +45,7 @@ public class AddSessionRooms {
 	/**
 	 * Create the application.
 	 */
-	public AddSessionRooms() {
+	public TimeTable_location() {
 		initialize();
 	}
 
@@ -51,19 +54,16 @@ public class AddSessionRooms {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setBounds(100, 40, 1350, 900);	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
 		frame.setResizable(false);
 		frame.getContentPane().setBackground(SystemColor.inactiveCaptionBorder);
 		frame.setBackground(Color.YELLOW);
-		frame.setTitle("Add Location");		
+		frame.setTitle("Time Table");		
 
-		// frame.setSize(1400, 2200);
-		frame.setBounds(100, 40, 1350, 900);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
 		
-	//	frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-
-	
 		
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 1344, 80);
@@ -83,51 +83,44 @@ public class AddSessionRooms {
 		frame.getContentPane().add(panel_1);
 		panel_1.setLayout(null);
 		
-		JButton btnAddLocation = new JButton("Add Location");
+		JButton btnAddLocation = new JButton("Lecturer");
 		btnAddLocation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AddLocation al = new AddLocation();
-				AddLocation.main(null);
-				frame.setVisible(false);
-
+				/**/
+				TimeTable_lec ar = new TimeTable_lec();
+				TimeTable_lec.main(null);
+				frame.setVisible(false);	 
+				 
+				
 			}
 		});
 		btnAddLocation.setBounds(12, 320, 238, 50);
 		btnAddLocation.setFont(new Font("Tahoma", Font.BOLD, 20));
 		panel_1.add(btnAddLocation);
 		
-		JButton btnManageLocation = new JButton("Manage Location");
+		JButton btnManageLocation = new JButton("Location");
 		btnManageLocation.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				ManageLocations ml = new ManageLocations();
-				ManageLocations.main(null);
-				frame.setVisible(false);	
+				//TimeTable_location ml = new TimeTable_location();
+			//	TimeTable_location.main(null);
+			//	frame.setVisible(false);	
 			}
 		});
-		btnManageLocation.setBounds(12, 383, 238, 50);
+		btnManageLocation.setBounds(12, 383, 252, 50);
 		btnManageLocation.setFont(new Font("Tahoma", Font.BOLD, 20));
 		panel_1.add(btnManageLocation);
 		
-		JButton btnAddSessionRooms = new JButton("Add Session Rooms");
+		JButton btnAddSessionRooms = new JButton("Student");
 		btnAddSessionRooms.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				TimeTable_tag ar = new TimeTable_tag();
+				TimeTable_tag.main(null);
+				frame.setVisible(false);	
 			}
 		});
-		btnAddSessionRooms.setBounds(12, 446, 252, 50);
+		btnAddSessionRooms.setBounds(12, 446, 238, 50);
 		btnAddSessionRooms.setFont(new Font("Tahoma", Font.BOLD, 20));
 		panel_1.add(btnAddSessionRooms);
-		
-		JButton btnManageSessionRooms = new JButton("Manage Session Rooms");
-		btnManageSessionRooms.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				ViewRooms msr = new ViewRooms();
-				ViewRooms.main(null);
-				frame.setVisible(false);
-			}
-		});
-		btnManageSessionRooms.setBounds(10, 520, 240, 50);
-		btnManageSessionRooms.setFont(new Font("Tahoma", Font.BOLD, 17));
-		panel_1.add(btnManageSessionRooms);
 		
 		JButton btnViewGroups_1 = new JButton("<<Back");
 		btnViewGroups_1.addActionListener(new ActionListener() {
@@ -148,7 +141,7 @@ public class AddSessionRooms {
 		panel_1.add(lbli);
 		
 	
-		JLabel lblNewLabel_1 = new JLabel("Add Session for Rooms");
+		JLabel lblNewLabel_1 = new JLabel("Time Table");
 		lblNewLabel_1.setBounds(262, 93, 1082, 48);
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 35));
@@ -160,60 +153,47 @@ public class AddSessionRooms {
 		separator.setBackground(SystemColor.controlText);
 		frame.getContentPane().add(separator);
 		
-		JLabel lblNewLabel_2 = new JLabel("Select Session");
-		lblNewLabel_2.setBounds(274, 268, 163, 25);
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 20));
-		frame.getContentPane().add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_2_1 = new JLabel("Select Room");
-		lblNewLabel_2_1.setBounds(274, 519, 134, 25);
-		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 20));
-		frame.getContentPane().add(lblNewLabel_2_1);
-		
 		JPanel panel_3 = new JPanel();
 		panel_3.setBounds(262, 658, 1082, 124);
 		panel_3.setBackground(Color.LIGHT_GRAY);
 		frame.getContentPane().add(panel_3);
 		panel_3.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(320, 281, 985, 476);
+		panel_3.add(scrollPane);
 		
-		JButton btnClear = new JButton("Submit");
-		btnClear.setBounds(268, 42, 238, 50);
-		panel_3.add(btnClear);
-		btnClear.setFont(new Font("Tahoma", Font.BOLD, 20));
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Time Slot", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"
+			}
+		));
+		scrollPane.setViewportView(table);
 		
-		JButton btnSave = new JButton("Clear");
-		btnSave.setBounds(518, 42, 238, 50);
-		panel_3.add(btnSave);
-		btnSave.setFont(new Font("Tahoma", Font.BOLD, 20));
+		JLabel lblNewLabel_2 = new JLabel("Location");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNewLabel_2.setBounds(345, 218, 171, 38);
+		panel_3.add(lblNewLabel_2);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(499, 519, 213, 33);
-		frame.getContentPane().add(comboBox);
+		comboBox.setBounds(552, 226, 413, 27);
+		panel_3.add(comboBox);
 		
-		JRadioButton rdbtnMSR = new JRadioButton("Consecutive Sessions");
-		rdbtnMSR.setBounds(467, 274, 273, 33);
-		rdbtnMSR.setFont(new Font("Tahoma", Font.BOLD, 20));
-		frame.getContentPane().add(rdbtnMSR);
+		JButton btnGenerate = new JButton("Generate");
+		btnGenerate.setBackground(Color.BLUE);
+		btnGenerate.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnGenerate.setBounds(1009, 218, 139, 42);
+		panel_3.add(btnGenerate);
 		
-		JRadioButton rdbtnParallelSessions = new JRadioButton("Parrallel Sessions");
-		rdbtnParallelSessions.setBounds(786, 274, 255, 33);
-		rdbtnParallelSessions.setFont(new Font("Tahoma", Font.BOLD, 20));
-		frame.getContentPane().add(rdbtnParallelSessions);
+		JButton btnNewButton = new JButton("Print");
+		btnNewButton.setBackground(Color.RED);
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 17));
+		btnNewButton.setBounds(1171, 218, 134, 42);
+		panel_3.add(btnNewButton);
+
 		
-		JRadioButton rdbtnNonOverlapping = new JRadioButton("Non Overlapping");
-		rdbtnNonOverlapping.setBounds(1070, 268, 207, 37);
-		rdbtnNonOverlapping.setFont(new Font("Tahoma", Font.BOLD, 20));
-		frame.getContentPane().add(rdbtnNonOverlapping);
-		
-		JLabel lblNewLabel_3 = new JLabel("Select Session");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblNewLabel_3.setBounds(274, 419, 156, 33);
-		frame.getContentPane().add(lblNewLabel_3);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(499, 415, 404, 37);
-		frame.getContentPane().add(comboBox_1);
-		
-		
-	}	
+	}
 }
